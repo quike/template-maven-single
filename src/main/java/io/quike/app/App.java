d66ea9b47c64a0f27d9731c902b58198acb107f2
quike <quike.mora@gmail.com>
2019-10-06 19:07:36 -0400

    Update unit tests framework to JUnit 5

--- src/main/java/io/quike/app/App.java
+++ src/main/java/io/quike/app/App.java
@@ -1,11 +1,20 @@
 package io.quike.app;
 
+import lombok.extern.log4j.Log4j2;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@Log4j2
 public class App {
 
-  private static int counter = 0;
+  private static int counter;
 
   public static void main(String[] args) {
-    counter++;
+    if (Objects.isNull(args)) throw new IllegalArgumentException("No null args are allowed.");
+    counter = 0;
+    log.info("args={}", Arrays.toString(args));
+    counter = args.length;
   }
 
   public static int getCounter() {
